fd774df3153bd68c1e9cb258caf258c04dd950f0
Parmeet Singh <psparmeet14@gmail.com>
2022-12-27 16:55:09 +0530

    feat: added recipe service & shopping-list service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe("My Test Recipe", "This is simply a Test.", "assets/0.jpg"),
-    new Recipe("My Test Recipe 2", "This is simply a Test 2.", "assets/0.jpg")
-  ];
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
 
+  constructor(private recipeService: RecipeService) {}
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
+
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,25 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+    private recipes: Recipe[] = [
+        new Recipe("My Test Recipe", "This is simply a Test.", "assets/0.jpg",
+         [new Ingredient('Brown Bread', 1), new Ingredient('Butter', 2)]),
+        new Recipe("My Test Recipe 2", "This is simply a Test 2.", "assets/0.jpg",
+         [new Ingredient('French Fries', 15), new Ingredient('Cheese', 3)])
+      ];
+
+    constructor(private shoppingListService: ShoppingListService) { }
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+    }
+}
