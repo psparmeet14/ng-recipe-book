caadae394e3622bdfa4ce94792673a29caf058b8
Parmeet Singh <psparmeet14@gmail.com>
2023-01-23 19:04:16 +0530

    feat: adding auth component and adding form in shopping-edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthComponent } from "./auth/auth.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
@@ -7,24 +8,13 @@ import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
-    {
-        path: "",
-        redirectTo: "/recipes",
-        pathMatch: 'full'
-    },
+    { path: "", redirectTo: "/recipes", pathMatch: 'full' },
     {
         path: "recipes",
         component: RecipesComponent,
-        children: 
-        [
-            {
-                path: "",
-                component: RecipeStartComponent
-            },
-            {
-                path: "new",
-                component: RecipeEditComponent
-            },
+        children: [
+            { path: "", component: RecipeStartComponent },
+            { path: "new", component: RecipeEditComponent },
             {
                 path: ":id",
                 component: RecipeDetailComponent
@@ -35,10 +25,8 @@ const appRoutes: Routes = [
             }
         ]
     },
-    {
-        path: "shopping-list",
-        component: ShoppingListComponent
-    }
+    { path: "shopping-list", component: ShoppingListComponent },
+    { path: "auth", component: AuthComponent }
 ];
 
 @NgModule({
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,14 +9,37 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
   @ViewChild('f', {static: false}) shoppingEditForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.shoppingListService.startedEditing
+      .subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+          this.editedItem = this.shoppingListService.getIngredient(index);
+          this.shoppingEditForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
+        }
+      );
+  }
 
   onAddItem() {
     const name = this.shoppingEditForm.value.name;
     const amount = this.shoppingEditForm.value.amount;
     this.shoppingListService.addIngredient(new Ingredient(name, amount));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
