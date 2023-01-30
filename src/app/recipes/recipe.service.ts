778744ba1bf8bda38aab1b1f6efff48b2f04ef1f
Parmeet Singh <psparmeet14@gmail.com>
2023-01-30 22:25:11 +0530

    feat: Added recipe form validations and implemented onSubmit()

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,13 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
+
     private recipes: Recipe[] = [
         new Recipe("My Test Recipe", "This is simply a Test.", "assets/0.jpg",
          [new Ingredient('Brown Bread', 1), new Ingredient('Butter', 2)]),
@@ -24,5 +27,15 @@ export class RecipeService {
 
     getRecipeById(index: number): Recipe {
         return this.recipes[index];
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
     }
 }
